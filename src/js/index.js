69102b042c52b8b7d527a310c005955d84f30224
Andrey Eferemov <samo.dur.andre@gmail.com>
2023-01-26 07:13:47 +0300

    feat: переделал карточки, поместил текст во внутрь

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,7 @@
 import { burger } from './modules/_burger.js';
-// import { popUp } from "./modules/_popUp.js";
 import { pressUp } from "./modules/_pressUp.js";
 import { pressDawn } from "./modules/_pressDawn.js";
 
-// popUp();
 burger();
 pressUp();
 pressDawn();
