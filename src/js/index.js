ab170e2259321e492fd20e117f38eff20867fde2
Andrey Eferemov <samo.dur.andre@gmail.com>
2023-01-27 07:56:00 +0300

    fix: паралакс эффект фуллскрина для всех браузереов в том числе safari

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,10 @@ import { burger } from './modules/_burger.js';
 import { pressUp } from "./modules/_pressUp.js";
 import { pressDawn } from "./modules/_pressDawn.js";
 import { initSlider } from "./modules/_initSlider.js";
+import { modalImage } from "./modules/_modalImage.js";
 
 burger();
 pressUp();
 pressDawn();
 initSlider();
+modalImage();
